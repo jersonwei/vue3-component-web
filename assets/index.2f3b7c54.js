78c04608d62e1103928235adeed3ceaa10c2590a
jesonwei <872937094@qq.com>
2023-02-15 03:54:11 +0800

    🎉 init(root): change file

---
+++ assets/index.2f3b7c54.js
@@ -0,0 +1,1 @@
+import{d as a,a as s,o as c,f as r,u as t}from"./index.a17dfc20.js";const m=a({__name:"index",setup(_){let o=e=>{console.log(e)};return(e,l)=>{const n=s("w-choose-area");return c(),r(n,{onChange:t(o)},null,8,["onChange"])}}});export{m as default};
--- assets/index.7d69da7c.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,a,o as c,c as o,b as r}from"./index.4fe5b90f.js";const t={};function s(_,l){const e=a("w-calendar");return c(),o("div",null,[r(e)])}var f=n(t,[["render",s]]);export{f as default};
